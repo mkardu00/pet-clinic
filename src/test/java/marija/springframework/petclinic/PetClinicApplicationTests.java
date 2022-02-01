4691573e4f632100315d3690d4357f30b8412091
Marija Kilic <marijakilic@Marijas-MacBook-Pro.local>
2022-02-01 17:44:57 +0100

    Initial commit

---
+++ src/test/java/marija/springframework/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package marija.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
