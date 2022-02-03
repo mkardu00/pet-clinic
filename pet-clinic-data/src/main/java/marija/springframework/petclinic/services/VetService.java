ebeba6662543707df8bf0338a402c03f2783f97f
marijakilic <mkardu00@fesb.hr>
2022-02-03 16:27:43 +0100

    Create Interfaces for Pet Clinic Services. Closes #5

---
+++ pet-clinic-data/src/main/java/marija/springframework/petclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package marija.springframework.petclinic.services;
+
+import marija.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
